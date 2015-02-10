cdecbccea7cd4857d8c58f1ef215120beb0f3dfb
AT4 wireless <at4_allseen@at4wireless.com>
2015-02-10 08:47:24 +0100

    Added support for new interfaces located under 'org.allseen' path to make Test Case About v1-05 properly work with recently added services
    Change-Id: I81aa5649fe903767e7b722c98aa5f224a8282bb9
    Signed-off-by: AT4 wireless <at4_allseen@at4wireless.com>

--- java/components/validation-tests/HEAD/validation-tests-utils/src/main/java/org/alljoyn/validation/testing/utils/InterfaceValidator.java
+++ java/components/validation-tests/HEAD/validation-tests-utils/src/main/java/org/alljoyn/validation/testing/utils/InterfaceValidator.java
@@ -85,13 +85,13 @@ public class InterfaceValidator
 
     List<String> getXmlFilesToBeLoaded()
     {
-        return Arrays.asList(IntrospectionXmlFile.About.getValue(), IntrospectionXmlFile.Action.getValue(), IntrospectionXmlFile.Audio.getValue(),
+    	return Arrays.asList(IntrospectionXmlFile.About.getValue(), IntrospectionXmlFile.Action.getValue(), IntrospectionXmlFile.Audio.getValue(),
                 IntrospectionXmlFile.Config.getValue(), IntrospectionXmlFile.Container.getValue(), IntrospectionXmlFile.ControlPanel.getValue(),
                 IntrospectionXmlFile.DeviceIcon.getValue(), IntrospectionXmlFile.Dialog.getValue(), IntrospectionXmlFile.HTTPControl.getValue(),
                 IntrospectionXmlFile.LabelProperty.getValue(), IntrospectionXmlFile.ListProperty.getValue(), IntrospectionXmlFile.Notification.getValue(),
                 IntrospectionXmlFile.NotificationAction.getValue(), IntrospectionXmlFile.Onboarding.getValue(), IntrospectionXmlFile.Peer.getValue(),
-                IntrospectionXmlFile.Property.getValue(),IntrospectionXmlFile.Clock.getValue(), IntrospectionXmlFile.TimeAuthority.getValue(), 
-                IntrospectionXmlFile.Alarm.getValue(), IntrospectionXmlFile.AlarmFactory.getValue(),IntrospectionXmlFile.Timer.getValue(),IntrospectionXmlFile.TimerFactory.getValue());
+                IntrospectionXmlFile.Property.getValue()IntrospectionXmlFile.Lighting.getValue(),IntrospectionXmlFile.GWAgentCtrlAcl.getValue(),
+                IntrospectionXmlFile.GWAgentCtrlAclMgmt.getValue(),IntrospectionXmlFile.GWAgentCtrlApp.getValue(),IntrospectionXmlFile.GWAgentCtrlAppMgmt.getValue());
     }
 
     private ValidationResult validateInterface(IntrospectionInterface standardizedIntrospectionInterface, String path)
