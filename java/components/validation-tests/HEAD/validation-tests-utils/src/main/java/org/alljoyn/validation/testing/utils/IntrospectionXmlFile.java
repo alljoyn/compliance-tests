0d81d25172383a74801d016f2d0727b92f5a93c8
Aaron Pulliam <aaronp@qce.qualcomm.com>
2015-04-01 20:09:29 -0700

    Revert "ASACOMP-35: About-v1-05 needs to be updated with event and  actions interface definitions"
    This reverts commit 8c6ec0f8b42dc42d0c2fd21ccf39208ea2135d47.
    
    Change-Id: I7e4300f6f234e9cdf864b31c5b461142e2782f5a
    Signed-off-by: Aaron Pulliam <aaronp@qce.qualcomm.com>

--- java/components/validation-tests/HEAD/validation-tests-utils/src/main/java/org/alljoyn/validation/testing/utils/IntrospectionXmlFile.java
+++ java/components/validation-tests/HEAD/validation-tests-utils/src/main/java/org/alljoyn/validation/testing/utils/IntrospectionXmlFile.java
@@ -17,7 +17,7 @@ package org.alljoyn.validation.testing.utils;
 
 public enum IntrospectionXmlFile
 {
-    About("introspection-xml/About.xml"), Introspectable("introspection-xml/Introspectable.xml"), Action("introspection-xml/Action.xml"), Audio("introspection-xml/Audio.xml"), Config("introspection-xml/Config.xml"), Container(
+    About("introspection-xml/About.xml"), Action("introspection-xml/Action.xml"), Audio("introspection-xml/Audio.xml"), Config("introspection-xml/Config.xml"), Container(
             "introspection-xml/Container.xml"), ControlPanel("introspection-xml/ControlPanel.xml"), DeviceIcon("introspection-xml/DeviceIcon.xml"), Dialog(
             "introspection-xml/Dialog.xml"), HTTPControl("introspection-xml/HTTPControl.xml"), LabelProperty("introspection-xml/LabelProperty.xml"), ListProperty(
             "introspection-xml/ListProperty.xml"), Notification("introspection-xml/Notification.xml"), NotificationAction("introspection-xml/NotificationAction.xml"), Onboarding(
