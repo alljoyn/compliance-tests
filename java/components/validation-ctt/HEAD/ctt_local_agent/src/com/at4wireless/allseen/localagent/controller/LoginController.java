acd8d1a97dc424c5ef860da7fdbb478d02a57db9
AT4 wireless <at4_allseen@at4wireless.com>
2015-09-18 11:13:51 +0200

    CTT Local Agent v1.6.0
    Issues addressed: ASACOMP-82, ASACOMP-86, ASACOMP-92, ASACOMP-97.
    
    Change-Id: Icc6dc6029c92fd7841eb8951ae3510b78d436a98
    Signed-off-by: AT4 wireless <at4_allseen@at4wireless.com>

---
+++ java/components/validation-ctt/HEAD/ctt_local_agent/src/com/at4wireless/allseen/localagent/controller/LoginController.java
@@ -0,0 +1,134 @@
+/*******************************************************************************
+ *  Copyright AllSeen Alliance. All rights reserved.
+ *
+ *     Permission to use, copy, modify, and/or distribute this software for any
+ *     purpose with or without fee is hereby granted, provided that the above
+ *     copyright notice and this permission notice appear in all copies.
+ *
+ *     THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *     WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *     MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *     ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *     WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *     ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *     OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *******************************************************************************/
+package com.at4wireless.allseen.localagent.controller;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.GeneralSecurityException;
+import java.security.KeyFactory;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.crypto.SecretKey;
+import javax.xml.bind.DatatypeConverter;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.at4wireless.allseen.localagent.controller.common.ControllerCommons;
+import com.at4wireless.allseen.localagent.model.LoginModel;
+import com.at4wireless.allseen.localagent.model.common.FileEncryption;
+import com.sun.jersey.api.client.ClientHandlerException;
+import com.sun.jersey.api.client.ClientResponse;
+
+public class LoginController
+{
+	private static final Logger logger = LogManager.getLogger(LoginController.class.getName());
+	
+	private LoginModel loginModel;
+	private String authenticatedUser;
+	private String sessionToken;
+	private SecretKey cipherKey;
+	
+	public LoginController()
+	{
+		loginModel = new LoginModel();
+	}
+	
+	public boolean loginWithWebServer(String userToAuthenticate, String passwordUsedInAuthentication) throws ClientHandlerException, JSONException
+	{
+		ClientResponse loginResponse = loginModel.getTokenFromWebServer(userToAuthenticate, passwordUsedInAuthentication);
+		JSONObject receivedJson = new JSONObject(loginResponse.getEntity(String.class));
+
+		if (loginResponse.getStatus() == HttpStatus.SC_OK)
+		{
+				loginResponse = loginModel.refreshToken(receivedJson.optString("refresh_token"));
+				
+				if (loginResponse.getStatus() == HttpStatus.SC_OK)
+				{
+					JSONObject refreshJson = new JSONObject(loginResponse.getEntity(String.class));
+
+					this.authenticatedUser = userToAuthenticate;
+					this.sessionToken = refreshJson.optString("access_token");;
+					
+					return true;
+				}
+				else
+				{
+					logger.warn(String.format("Response code: %s", loginResponse.getStatus()));
+					return false;
+				}
+		}
+		else
+		{
+			logger.warn(String.format("Response code: %s", loginResponse.getStatus()));
+			return false;
+		}
+	}
+	
+	public void exchangeEncryption() throws GeneralSecurityException, URISyntaxException, IOException
+	{
+		FileEncryption fEncryption = new FileEncryption();
+		fEncryption.makeKeys();
+
+		X509EncodedKeySpec spec = KeyFactory.getInstance("RSA").getKeySpec(fEncryption.getRsaPublicKey(),X509EncodedKeySpec.class);
+		
+		String str = DatatypeConverter.printBase64Binary(spec.getEncoded());
+		
+		HttpResponse response = loginModel.exchangeCipherKeys(authenticatedUser, sessionToken, str);
+		
+		cipherKey = fEncryption.decryptAESwithRSA(ControllerCommons.HttpResponseToString(response));
+	}
+	
+	public List<Object> isTestToolUpdated(String testToolLocalVersion) throws URISyntaxException, IOException
+	{
+		HttpResponse responseFromServer = loginModel.isTestToolUpdated(testToolLocalVersion, sessionToken);
+		List<Object> list = new ArrayList<Object>();
+		String responseString = ControllerCommons.HttpResponseToString(responseFromServer);
+			
+		if (responseString.contains("new version available"))
+		{
+			list.add(false);
+			list.add(responseString.split(":")[1].substring(1));
+		}
+		else if (responseString.equals("version up to date"))
+		{
+			list.add(true);
+		}
+		
+		return list;
+	}
+	
+	public String getAuthenticatedUser()
+	{
+		return authenticatedUser;
+	}
+	
+	public String getSessionToken()
+	{
+		return sessionToken;
+	}
+	
+	public SecretKey getCipherKey()
+	{
+		return cipherKey;
+	}
+}
