cfaec7608446e1e0a99fac1e20047e81e5e4c69b
AT4 wireless <at4_allseen@at4wireless.com>
2015-09-18 13:20:32 +0200

    CTT Local Agent v1.6.0
    Issues addressed: ASACOMP-82, ASACOMP-86, ASACOMP-92, ASACOMP-97.
    
    Change-Id: Ib909557c41193dfea35e33f1495336253d6140f6
    Signed-off-by: AT4 wireless <at4_allseen@at4wireless.com>

---
+++ java/components/validation-ctt/HEAD/ctt_local_agent/src/com/at4wireless/allseen/localagent/view/common/ViewCommons.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ *  Copyright AllSeen Alliance. All rights reserved.
+ *
+ *     Permission to use, copy, modify, and/or distribute this software for any
+ *     purpose with or without fee is hereby granted, provided that the above
+ *     copyright notice and this permission notice appear in all copies.
+ *
+ *     THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *     WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *     MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *     ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *     WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *     ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *     OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *******************************************************************************/
+package com.at4wireless.allseen.localagent.view.common;
+
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ViewCommons
+{
+	private static final Logger logger = LogManager.getLogger(ViewCommons.class.getName());
+    public static Rectangle setCenteredRectangle(int rectangleWidth, int rectangleHeight)
+    {
+    	Dimension screenDimensions = Toolkit.getDefaultToolkit().getScreenSize();
+		int xCenter = (screenDimensions.width - rectangleWidth)/2;
+		int yCenter = (screenDimensions.height - rectangleHeight)/2;
+
+		return new Rectangle(xCenter, yCenter, rectangleWidth, rectangleHeight);
+    }
+    
+    public static Image readImageFromFile(String imagePath, String imageName)
+	{
+		Image image = null;
+		
+		try
+		{
+			image = ImageIO.read(new File(imagePath + File.separator + imageName));
+		}
+		catch (IOException e)
+		{
+			logger.warn(String.format("Resource %s not found", imageName));
+		}
+		
+		return image;
+	}
+}
