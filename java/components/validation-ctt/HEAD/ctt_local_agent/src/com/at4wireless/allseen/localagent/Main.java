df9f43d4d26d6e97359010da0094250fb9a5daef
AT4 wireless <at4_allseen@at4wireless.com>
2015-09-18 13:20:32 +0200

    CTT Local Agent v1.6.0
    Issues addressed: ASACOMP-82, ASACOMP-86, ASACOMP-92, ASACOMP-97.
    
    Change-Id: Ib909557c41193dfea35e33f1495336253d6140f6
    Signed-off-by: AT4 wireless <at4_allseen@at4wireless.com>

---
+++ java/components/validation-ctt/HEAD/ctt_local_agent/src/com/at4wireless/allseen/localagent/Main.java
@@ -0,0 +1,122 @@
+/*******************************************************************************
+ *  Copyright AllSeen Alliance. All rights reserved.
+ *
+ *     Permission to use, copy, modify, and/or distribute this software for any
+ *     purpose with or without fee is hereby granted, provided that the above
+ *     copyright notice and this permission notice appear in all copies.
+ *
+ *     THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *     WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *     MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *     ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *     WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *     ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *     OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *******************************************************************************/
+package com.at4wireless.allseen.localagent;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.LoggerConfig;
+
+import com.at4wireless.allseen.localagent.controller.common.ControllerCommons;
+import com.at4wireless.allseen.localagent.model.common.ConfigParameter;
+import com.at4wireless.allseen.localagent.view.LoginWindow;
+import com.at4wireless.allseen.localagent.view.ProjectsDetailsWindow;
+import com.at4wireless.allseen.localagent.view.common.ViewCommons;
+
+public class Main
+{
+	private static final Logger logger = LogManager.getLogger(Main.class.getName());
+	public static final String CTT_LOCAL_AGENT_VERSION = "v1.6.0";
+	public static final String VERSION_LABEL = "AllSeen Test Tool Local Agent " + CTT_LOCAL_AGENT_VERSION;
+	public static final String POWERED_BY_LABEL = "Powered by AT4 wireless";
+	private static final int WINDOW_WIDTH = 1024;
+	private static final int WINDOW_HEIGHT = 500;
+	
+	private static final String ICON = "ic_AllSeen.png"; 
+	
+	public static void main(String[] args)
+	{
+		SwingUtilities.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				logger.info(String.format("Initializing %s...", VERSION_LABEL));
+				
+				try
+				{
+					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+				}
+				catch (ClassNotFoundException e)
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				catch (InstantiationException e)
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				catch (IllegalAccessException e)
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				catch (UnsupportedLookAndFeelException e)
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				JFrame frame = new JFrame("CTT Local Agent");
+				frame.setIconImage(ViewCommons.readImageFromFile(ConfigParameter.RESOURCES_PATH, ICON));
+				
+				setLoggerLevel();
+				
+				LoginWindow loginWindow = new LoginWindow(frame, true);
+				
+				frame.setBounds(ViewCommons.setCenteredRectangle(WINDOW_WIDTH, WINDOW_HEIGHT));
+				loginWindow.setVisible(true);
+				
+				ProjectsDetailsWindow projectWindow = new ProjectsDetailsWindow(loginWindow);
+				
+				frame.add(projectWindow);
+				frame.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+				frame.addWindowListener(new WindowAdapter()
+				{
+					public void windowClosing(WindowEvent e)
+					{
+						ControllerCommons.clean();
+						System.exit(0);
+					}
+				});
+				frame.setResizable(false);
+				projectWindow.draw();
+			}
+		});
+	}
+	
+	private static void setLoggerLevel()
+	{
+		LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+		Configuration config = ctx.getConfiguration();
+		LoggerConfig loggerConfig = config.getLoggerConfig(LogManager.ROOT_LOGGER_NAME);
+		
+		logger.info(String.format("Setting logger level to: %s", ConfigParameter.LOG_LEVEL));
+		loggerConfig.setLevel(Level.toLevel(ConfigParameter.LOG_LEVEL));
+		ctx.updateLoggers();
+	}
+}
---
+++ java/components/validation-ctt/HEAD/ctt_local_agent/src/com/at4wireless/allseen/localagent/view/action/PleaseWaitAction.java
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ *  Copyright AllSeen Alliance. All rights reserved.
+ *
+ *     Permission to use, copy, modify, and/or distribute this software for any
+ *     purpose with or without fee is hereby granted, provided that the above
+ *     copyright notice and this permission notice appear in all copies.
+ *
+ *     THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *     WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *     MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *     ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *     WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *     ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *     OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *******************************************************************************/
+package com.at4wireless.allseen.localagent.view.action;
+
+import java.awt.BorderLayout;
+import java.awt.Dialog.ModalityType;
+import java.awt.event.ActionEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import javax.swing.AbstractAction;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.SwingWorker;
+
+public class PleaseWaitAction extends AbstractAction
+{
+	private static final long serialVersionUID = 1L;
+	protected static final long SLEEP_TIME = 3 * 1000;
+	private JFrame parentFrame;
+	private SwingWorker<?, Void> mySwingWorker;
+	private String textToDisplay;
+
+	public PleaseWaitAction(JFrame parent, SwingWorker<?, Void> mySwingWorker, String textToDisplay)
+	{
+		super("PleaseWaitAction");
+		this.parentFrame = parent;
+		this.mySwingWorker = mySwingWorker;
+		this.textToDisplay = textToDisplay;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent evt)
+	{
+		final JDialog dialog = new JDialog(parentFrame, "CTT Local Agent", ModalityType.APPLICATION_MODAL);
+
+		mySwingWorker.addPropertyChangeListener(new PropertyChangeListener()
+		{
+			@Override
+			public void propertyChange(PropertyChangeEvent evt)
+			{
+				if (evt.getPropertyName().equals("state"))
+				{
+					if (evt.getNewValue() == SwingWorker.StateValue.DONE)
+					{
+						dialog.dispose();
+					}
+				}
+			}
+		});
+		
+		mySwingWorker.execute();
+		JProgressBar progressBar = new JProgressBar();
+		progressBar.setIndeterminate(true);
+		JPanel panel = new JPanel(new BorderLayout());
+		panel.add(progressBar, BorderLayout.CENTER);
+		panel.add(new JLabel(textToDisplay), BorderLayout.PAGE_START);
+		dialog.add(panel);
+		dialog.pack();
+		dialog.setLocationRelativeTo(parentFrame);
+		dialog.setVisible(true);
+   }
+}
