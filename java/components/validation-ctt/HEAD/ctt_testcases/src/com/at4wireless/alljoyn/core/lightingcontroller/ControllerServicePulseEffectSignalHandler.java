a2c5481ab9ad0801d33a404d58a73c000a15d760
AT4 wireless <at4_allseen@at4wireless.com>
2015-12-10 14:39:33 +0100

    CTT Test Cases Package v15.09.00a R2
    Issues addressed: ASACOMP-148
    
    Change-Id: I3ad7cb14cafcef217abcd3d7ae7c48977e359017
    Signed-off-by: AT4 wireless <at4_allseen@at4wireless.com>

---
+++ java/components/validation-ctt/HEAD/ctt_testcases/src/com/at4wireless/alljoyn/core/lightingcontroller/ControllerServicePulseEffectSignalHandler.java
@@ -0,0 +1,69 @@
+/*******************************************************************************
+*  Copyright (c) AllSeen Alliance. All rights reserved.
+*
+*     Permission to use, copy, modify, and/or distribute this software for any
+*     purpose with or without fee is hereby granted, provided that the above
+*     copyright notice and this permission notice appear in all copies.
+*
+*     THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+*     WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+*     MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+*     ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+*     WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+*     ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+*     OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*******************************************************************************/
+package com.at4wireless.alljoyn.core.lightingcontroller;
+
+import org.alljoyn.bus.annotation.BusSignalHandler;
+
+public class ControllerServicePulseEffectSignalHandler
+{
+    static
+    {
+        System.loadLibrary("alljoyn_java");
+    }
+
+    private ControllerServiceSignalListener signalListener;
+
+    public void setUpdateListener(ControllerServiceSignalListener listener)
+    {
+        signalListener = listener;
+    }
+
+    @BusSignalHandler(iface = "org.allseen.LSF.ControllerService.PulseEffect", signal = "PulseEffectsNameChanged")
+    public void PulseEffectsNameChanged(String[] pulseEffectIDs)
+    {
+        if (signalListener != null)
+        {
+            signalListener.handlePulseEffectsNameChanged(pulseEffectIDs);
+        }
+    }
+
+    @BusSignalHandler(iface = "org.allseen.LSF.ControllerService.PulseEffect", signal = "PulseEffectsCreated")
+    public void PulseEffectsCreated(String[] pulseEffectIDs)
+    {
+        if (signalListener != null)
+        {
+            signalListener.handlePulseEffectsCreated(pulseEffectIDs);
+        }
+    }
+
+    @BusSignalHandler(iface = "org.allseen.LSF.ControllerService.PulseEffect", signal = "PulseEffectsUpdated")
+    public void PulseEffectsUpdated(String[] pulseEffectIDs)
+    {
+        if (signalListener != null)
+        {
+            signalListener.handlePulseEffectsUpdated(pulseEffectIDs);
+        }
+    }
+
+    @BusSignalHandler(iface = "org.allseen.LSF.ControllerService.PulseEffect", signal = "PulseEffectsDeleted")
+    public void PulseEffectsDeleted(String[] pulseEffectIDs)
+    {
+        if (signalListener != null)
+        {
+            signalListener.handlePulseEffectsDeleted(pulseEffectIDs);
+        }
+    }
+}
