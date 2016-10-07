303fa23fff80429dc95906252eb29856eca3d1be
AT4 wireless <at4_allseen@at4wireless.com>
2016-10-07 13:52:22 +0200

    Replace AllSeenAlliance License with Apache License Version 2.0
    http://www.apache.org/licenses/LICENSE-2.0
    
    * Executed in batch by
      https://cgit.allseenalliance.org/extras/automation.git/tree/relicense/bin/do-relicense
    * Patch review took place here:
      https://git.allseenalliance.org/gerrit/#/c/9377/
    
    * For each branch of each project of the AllJoyn Open Source Project
      gerrit repository
    - search / replace old license with new in all matching files
    - search / replace old copyright with new in all files which only
      include copyright
    - assign copyright to Open Connectivity Foundation, AllJoyn Open
      Source Project Contributors and others.
    - propose changes for review by commiters
    - include SPDX
    
    Change-Id: Ia55083c28177df3a41d668c84e9951d9cbc423fb
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

--- cpp/components/validation-ctt/HEAD/ctt_testcases/ServiceHelper.cpp
+++ cpp/components/validation-ctt/HEAD/ctt_testcases/ServiceHelper.cpp
@@ -1,17 +1,30 @@
 /******************************************************************************
-* Copyright AllSeen Alliance. All rights reserved.
+* *    Copyright (c) Open Connectivity Foundation (OCF) and AllJoyn Open
+*    Source Project (AJOSP) Contributors and others.
 *
-*    Permission to use, copy, modify, and/or distribute this software for any
-*    purpose with or without fee is hereby granted, provided that the above
-*    copyright notice and this permission notice appear in all copies.
+*    SPDX-License-Identifier: Apache-2.0
 *
-*    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-*    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-*    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-*    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-*    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-*    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-*    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*    All rights reserved. This program and the accompanying materials are
+*    made available under the terms of the Apache License, Version 2.0
+*    which accompanies this distribution, and is available at
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+*    Copyright (c) Open Connectivity Foundation and Contributors to AllSeen
+*    Alliance. All rights reserved.
+*
+*    Permission to use, copy, modify, and/or distribute this software for
+*    any purpose with or without fee is hereby granted, provided that the
+*    above copyright notice and this permission notice appear in all
+*    copies.
+*
+*     THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+*     WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+*     WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+*     AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+*     DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+*     PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+*     TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+*     PERFORMANCE OF THIS SOFTWARE.
 ******************************************************************************/
 #include "stdafx.h"
 #include "ServiceHelper.h"
@@ -28,6 +41,15 @@ using namespace services;
 using namespace std;
 
 uint32_t ServiceHelper::LINK_TIMEOUT_IN_SECONDS = 120;
+
+static void DebugOut(DbgMsgType type, const char* module, const char* msg, void* context)
+{
+    QCC_UNUSED(type);
+    QCC_UNUSED(module);
+    QCC_UNUSED(msg);
+    QCC_UNUSED(context);
+    // Do nothing to suppress AJ error and debug prints
+}
 
 QStatus ServiceHelper::initializeClient(const string& t_BusApplicationName,
 	const string& t_DeviceId, const uint8_t* t_AppId)
@@ -69,6 +91,10 @@ QStatus ServiceHelper::initialize(const string& t_BusApplicationName,
 		AllJoynShutdown();
 		return status;
 	}
+
+#if NDEBUG
+    QCC_RegisterOutputCallback(DebugOut, NULL);
+#endif
 
 	//--------------------------------------------------
 	// BUS ATTACHMENT INITIALIZATION
