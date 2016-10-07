6dd412ded7940b4fa8116c6af60fcfd2c36934d1
AT4 wireless <at4_allseen@at4wireless.com>
2016-10-07 13:52:22 +0200

    Replace AllSeenAlliance License with Apache License Version 2.0
    http://www.apache.org/licenses/LICENSE-2.0
    
    * Executed in batch by
      https://cgit.allseenalliance.org/extras/automation.git/tree/relicense/bin/do-relicense
    * Patch review took place here:
      https://git.allseenalliance.org/gerrit/#/c/9377/
    
    * For each branch of each project of the AllJoyn Open Source Project
      gerrit repository
    - search / replace old license with new in all matching files
    - search / replace old copyright with new in all files which only
      include copyright
    - assign copyright to Open Connectivity Foundation, AllJoyn Open
      Source Project Contributors and others.
    - propose changes for review by commiters
    - include SPDX
    
    Change-Id: Ia55083c28177df3a41d668c84e9951d9cbc423fb
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

---
+++ cpp/components/validation-ctt/HEAD/ctt_testcases/CdmTestSuite.h
@@ -0,0 +1,102 @@
+/******************************************************************************
+* * Copyright (c) Open Connectivity Foundation (OCF) and AllJoyn Open
+*    Source Project (AJOSP) Contributors and others.
+*
+*    SPDX-License-Identifier: Apache-2.0
+*
+*    All rights reserved. This program and the accompanying materials are
+*    made available under the terms of the Apache License, Version 2.0
+*    which accompanies this distribution, and is available at
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+*    Copyright (c) Open Connectivity Foundation and Contributors to AllSeen
+*    Alliance. All rights reserved.
+*
+*    Permission to use, copy, modify, and/or distribute this software for
+*    any purpose with or without fee is hereby granted, provided that the
+*    above copyright notice and this permission notice appear in all
+*    copies.
+*
+*     THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+*     WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+*     WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+*     AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+*     DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+*     PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+*     TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+*     PERFORMANCE OF THIS SOFTWARE.
+******************************************************************************/
+#pragma once
+
+#include "IOManager.h"
+#include "ServiceHelper.h"
+
+#include <iostream>
+#include <vector>
+
+#include <alljoyn/cdm/DeviceListener.h>
+#include <alljoyn/cdm/CdmController.h>
+#include <qcc\Event.h>
+
+#define TIMEOUT 30000
+
+using namespace ajn;
+using namespace services;
+
+#define TEST_LOG_1(logMsg)  \
+    LOG(INFO) << ++LOG_NO << ". " << logMsg
+#define TEST_LOG_2(logMsg)  \
+    LOG(INFO) << "  * " << logMsg
+#define TEST_LOG_3(logMsg)  \
+    LOG(INFO) << "    - " << logMsg
+#define TEST_LOG_OBJECT_PATH(logMsg)  \
+    LOG(INFO) << "[ObjectPath : " << logMsg << "]"
+
+class InterfaceInfo
+{
+public:
+	std::string busName;
+	std::string deviceName;
+	ajn::SessionPort sessionPort;
+	ajn::SessionId sessionId;
+	std::string objectPath;
+	ajn::services::CdmAboutData aboutData;
+	ajn::AboutObjectDescription aboutDescription;
+
+	InterfaceInfo()
+	{
+	}
+	InterfaceInfo(const char* name, ajn::SessionPort port, const char* path, ajn::services::CdmAboutData& data, ajn::AboutObjectDescription& description);
+};
+
+class CdmTestSuite : public ::testing::Test, public IOManager, public ajn::services::DeviceListener, public ajn::SessionListener
+{
+public:
+	CdmTestSuite();
+	void SetUp();
+	void TearDown();
+
+	virtual void OnDeviceAdded(const char* busname, ajn::SessionPort port, const ajn::services::CdmAboutData& data, const ajn::AboutObjectDescription& description);
+	virtual void OnDeviceRemoved(const char* busname);
+	virtual void OnDeviceSessionJoined(const ajn::services::DeviceInfoPtr& info);
+	virtual void OnDeviceSessionLost(ajn::SessionId sessionId);
+
+	QStatus WaitForControllee(ajn::services::CdmInterfaceType type = (ajn::services::CdmInterfaceType)-1);
+protected:
+	static AJ_PCSTR BUS_APPLICATION_NAME;
+	static AJ_PCSTR ALL_CDM_INTERFACE;
+
+	std::string m_DutDeviceId = std::string{ "" };
+	uint8_t* m_DutAppId{ nullptr };
+	uint32_t m_AnnouncementTimeout;
+	ServiceHelper* m_ServiceHelper{ nullptr };
+	AboutAnnouncementDetails* m_DeviceAboutAnnouncement{ nullptr };
+
+	ajn::services::CdmController* m_controller;
+	qcc::String m_interfaceNameForTest;
+	std::vector<InterfaceInfo> m_interfaces;
+	qcc::Event m_eventOnDeviceAdded;
+	int LOG_NO;
+
+	void releaseResources();
+};
--- cpp/components/validation-ctt/HEAD/ctt_testcases/IOManager.cpp
+++ cpp/components/validation-ctt/HEAD/ctt_testcases/IOManager.cpp
@@ -1,17 +1,30 @@
 /******************************************************************************
-* Copyright AllSeen Alliance. All rights reserved.
+* * Copyright (c) Open Connectivity Foundation (OCF) and AllJoyn Open
+*    Source Project (AJOSP) Contributors and others.
 *
-*    Permission to use, copy, modify, and/or distribute this software for any
-*    purpose with or without fee is hereby granted, provided that the above
-*    copyright notice and this permission notice appear in all copies.
+*    SPDX-License-Identifier: Apache-2.0
 *
-*    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-*    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-*    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-*    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-*    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-*    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-*    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*    All rights reserved. This program and the accompanying materials are
+*    made available under the terms of the Apache License, Version 2.0
+*    which accompanies this distribution, and is available at
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+*    Copyright (c) Open Connectivity Foundation and Contributors to AllSeen
+*    Alliance. All rights reserved.
+*
+*    Permission to use, copy, modify, and/or distribute this software for
+*    any purpose with or without fee is hereby granted, provided that the
+*    above copyright notice and this permission notice appear in all
+*    copies.
+*
+*     THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+*     WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+*     WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+*     AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+*     DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+*     PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+*     TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+*     PERFORMANCE OF THIS SOFTWARE.
 ******************************************************************************/
 #include "stdafx.h"
 #include "IOManager.h"
@@ -105,9 +118,9 @@ void IOManager::printIcs(ServiceFramework t_ServiceFramework)
 			filters.push_back("ICSLC");
 			break;
 		}
-		case(ServiceFramework::HAE) :
+		case(ServiceFramework::CDM) :
 		{
-			filters.push_back("ICSH");
+			filters.push_back("ICSCDM");
 			break;
 		}
 	}
@@ -161,9 +174,9 @@ void IOManager::printIxit(ServiceFramework t_ServiceFramework)
 			filters.push_back("IXITLC");
 			break;
 		}
-		case(ServiceFramework::HAE):
+		case(ServiceFramework::CDM):
 		{
-			filters.push_back("IXITH");
+			filters.push_back("IXITCDM");
 			break;
 		}
 	}
@@ -216,9 +229,9 @@ void IOManager::printGeneralParameter(ServiceFramework t_ServiceFramework)
 			filters.push_back("GPLC");
 			break;
 		}
-		case(ServiceFramework::HAE):
+		case(ServiceFramework::CDM):
 		{
-			filters.push_back("GPH");
+			filters.push_back("GPCDM");
 			break;
 		}
 	}
