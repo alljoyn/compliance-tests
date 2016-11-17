68143d28f7199909cb834cb7e0e29b1a5fbbe1ad
AT4 wireless <at4_allseen@at4wireless.com>
2016-11-17 11:34:45 +0100

    Replace AllSeenAlliance License with Apache License Version 2.0
    http://www.apache.org/licenses/LICENSE-2.0
    
    * Executed in batch by
      https://cgit.allseenalliance.org/extras/automation.git/tree/relicense/bin/do-relicense
    * Patch review took place here:
      https://git.allseenalliance.org/gerrit/#/c/9377/
    
    * For each branch of each project of the AllJoyn Open Source Project
      gerrit repository
    - search / replace old license with new in all matching files
    - search / replace old copyright with new in all files which only
      include copyright
    - assign copyright to Open Connectivity Foundation, AllJoyn Open
      Source Project Contributors and others.
    - propose changes for review by commiters
    - include SPDX
    
    Change-Id: If155dc396623ad18f5171cd76b33f671abff5b3f
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

--- java/components/validation-ctt/HEAD/ctt_web_server/src/main/java/com/at4wireless/spring/model/User.java
+++ java/components/validation-ctt/HEAD/ctt_web_server/src/main/java/com/at4wireless/spring/model/User.java
@@ -1,17 +1,30 @@
 /*******************************************************************************
- * Copyright AllSeen Alliance. All rights reserved.
+ *  *      Copyright (c) Open Connectivity Foundation (OCF) and AllJoyn Open
+ *      Source Project (AJOSP) Contributors and others.
  *
- *      Permission to use, copy, modify, and/or distribute this software for any
- *      purpose with or without fee is hereby granted, provided that the above
- *      copyright notice and this permission notice appear in all copies.
- *      
- *      THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- *      WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- *      MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- *      ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- *      WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- *      ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- *      OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *      SPDX-License-Identifier: Apache-2.0
+ *
+ *      All rights reserved. This program and the accompanying materials are
+ *      made available under the terms of the Apache License, Version 2.0
+ *      which accompanies this distribution, and is available at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *      Copyright (c) Open Connectivity Foundation and Contributors to AllSeen
+ *      Alliance. All rights reserved.
+ *
+ *      Permission to use, copy, modify, and/or distribute this software for
+ *      any purpose with or without fee is hereby granted, provided that the
+ *      above copyright notice and this permission notice appear in all
+ *      copies.
+ *
+ *       THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ *       WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ *       WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+ *       AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+ *       DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+ *       PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ *       TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *       PERFORMANCE OF THIS SOFTWARE.
  *******************************************************************************/
 package com.at4wireless.spring.model;
 
@@ -27,6 +40,16 @@ import javax.validation.constraints.Size;
 @Table(name = "users")
 public class User
 {
+	public User() {}
+	public User(String user, String password, String repPassword, String role, String aesSecretKey)
+	{
+		this.user = user;
+		this.password = password;
+		this.repPassword = repPassword;
+		this.role = role;
+		this.aesSecretKey = aesSecretKey;
+	}
+	
 	@Id @Column(name = "username", nullable = false)
 	@Size(min=5, max=13)
 	private String user;
@@ -34,7 +57,6 @@ public class User
 	public void setUser(String user) { this.user = user; }
 	
 	@Column(name = "password", nullable = false)
-	@Size(min=5, max=13)
 	private String password;
 	public String getPassword() { return password; }
 	public void setPassword(String password) { this.password = password; }
