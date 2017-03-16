b74a2b525e25987bb04303bbb1b1d46d19a48e44
AT4 wireless <at4_allseen@at4wireless.com>
2017-03-16 15:26:38 +0100

    CTT Local Agent v2.3.0
    Changed target url to new one
    
    Change-Id: I694d48bef7a512ed47727ba45e43b878d50a4059
    Signed-off-by: AT4 wireless <at4_allseen@at4wireless.com>

--- java/components/validation-ctt/HEAD/ctt_local_agent/src/com/at4wireless/alljoyn/localagent/Main.java
+++ java/components/validation-ctt/HEAD/ctt_local_agent/src/com/at4wireless/alljoyn/localagent/Main.java
@@ -1,31 +1,18 @@
 /*******************************************************************************
- *     Copyright (c) Open Connectivity Foundation (OCF), AllJoyn Open Source
- *     Project (AJOSP) Contributors and others.
- *     
- *     SPDX-License-Identifier: Apache-2.0
- *     
- *     All rights reserved. This program and the accompanying materials are
- *     made available under the terms of the Apache License, Version 2.0
- *     which accompanies this distribution, and is available at
- *     http://www.apache.org/licenses/LICENSE-2.0
- *     
- *     Copyright (c) Open Connectivity Foundation and Contributors to AllSeen
- *     Alliance. All rights reserved.
- *     
- *     Permission to use, copy, modify, and/or distribute this software for
- *     any purpose with or without fee is hereby granted, provided that the
- *     above copyright notice and this permission notice appear in all
- *     copies.
- *     
- *     THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
- *     WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
- *     WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
- *     AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
- *     DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
- *     PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
- *     TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
- *     PERFORMANCE OF THIS SOFTWARE.
-*******************************************************************************/
+ *  Copyright AllSeen Alliance. All rights reserved.
+ *
+ *     Permission to use, copy, modify, and/or distribute this software for any
+ *     purpose with or without fee is hereby granted, provided that the above
+ *     copyright notice and this permission notice appear in all copies.
+ *
+ *     THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *     WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *     MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *     ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *     WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *     ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *     OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *******************************************************************************/
 package com.at4wireless.alljoyn.localagent;
 
 import java.awt.event.WindowAdapter;
@@ -53,7 +40,7 @@ import com.at4wireless.alljoyn.localagent.view.common.ViewCommons;
 public class Main
 {
 	private static final Logger logger = LogManager.getLogger(Main.class.getName());
-	public static final String CTT_LOCAL_AGENT_VERSION = "v2.2.0";
+	public static final String CTT_LOCAL_AGENT_VERSION = "v2.3.0";
 	public static final String VERSION_LABEL = "AllSeen Test Tool Local Agent " + CTT_LOCAL_AGENT_VERSION;
 	public static final String POWERED_BY_LABEL = "Powered by AT4 wireless";
 	private static final int WINDOW_WIDTH = 1024;
@@ -132,4 +119,4 @@ public class Main
 		loggerConfig.setLevel(Level.toLevel(ConfigParameter.LOG_LEVEL));
 		ctx.updateLoggers();
 	}
-}
+}
